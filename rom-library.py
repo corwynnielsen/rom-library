1b56a7066f7e57a3d9d83e0cbd5273f4e7857951
Corwyn Nielsen <corwynnielsen@gmail.com>
2024-11-06 14:49:44 -0700

    added psvita, a lot of refactoring, removing/updating comments, updating gitignore, added prints

--- rom-library.py
+++ rom-library.py
@@ -1,140 +1,113 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from pathlib import Path
 
 roms_dir = '/run/media/SN01T/Emulation/roms/'
+systems_xml_path = '~/ES-DE/custom_systems/es_systems.xml'
 
-# Based on your actual url that you use to download games off of, change the url below to match your urls. I am not able to openly post the links I use due to potential legal issues of downloading roms off the internet.
-snes_url = 'XXXXXXX'
-nes_url  = 'XXXXXXX'
-n64_url  = 'XXXXXXX'
-gb_url   = 'XXXXXXX'
-gbc_url  = 'XXXXXXX'
-gba_url  = 'XXXXXXX'
-gc_url   = 'XXXXXXX'
-nds_url  = 'XXXXXXX'
-n3ds_url = 'XXXXXXX'
-wii_url  = 'XXXXXXX'
-wiiu_url = 'XXXXXXX'
-psx_url  = 'XXXXXXX'
-ps2_url  = 'XXXXXXX'
-psp_url  = 'XXXXXXX'
-dc_url   = 'XXXXXXX'
+# Based on the actual ROM download url, change the url below to correct urls.
+snes_url   = 'XXXXXXX'
+nes_url    = 'XXXXXXX'
+n64_url    = 'XXXXXXX'
+gb_url     = 'XXXXXXX'
+gbc_url    = 'XXXXXXX'
+gba_url    = 'XXXXXXX'
+gc_url     = 'XXXXXXX'
+nds_url    = 'XXXXXXX'
+n3ds_url   = 'XXXXXXX'
+wii_url    = 'XXXXXXX'
+wiiu_url   = 'XXXXXXX'
+psx_url    = 'XXXXXXX'
+ps2_url    = 'XXXXXXX'
+psp_url    = 'XXXXXXX'
+psvita_url = 'XXXXXXX'
+dc_url     = 'XXXXXXX'
+rom_urls = [
+    snes_url, nes_url, n64_url, gb_url, gbc_url, gba_url ,gc_url, nds_url,
+    n3ds_url, wii_url, wiiu_url, psx_url ,ps2_url,psp_url,psvita_url, dc_url
+]
 
 def read_files(url):
     # Send a GET request to the URL
     response = requests.get(url)
-
-    # Check if the request was successful
     if response.status_code == 200:
-        # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(response.content, 'html.parser')
         # Find al the <a> tags with href attributes
         links = soup.find_all('a', href=True)
+        print(f'Response received from url {url} with {len(links)} unfiltered links')
 
-        # Include and not include texts to filter out. Feel free to remove/add as necessary.
         usaTag = '(USA'
-        excludedWords = ['(Beta)', '(Beta 1)', '(Beta 2)', '(Beta 3)', '(Rev 1)', '(Arcade)', '(Proto)', '(Proto 1)', '(Proto 2)', '(Sample)', '(Rev 2)', '(Rev 3)', '(Competition Cart', '(Pirate)', '(Demo)']
+        filteredWords = ['(Beta)', '(Beta 1)', '(Beta 2)', '(Beta 3)', '(Rev 1)', '(Arcade)', '(Proto)', '(Proto 1)', '(Proto 2)', '(Sample)', '(Rev 2)', '(Rev 3)', '(Competition Cart', '(Pirate)', '(Demo)']
+        accepted_file_extensions = ['.zip', '.7z', '.wua']
 
-        # Filter only USA full version files
+        # Filter only USA full version files + allowed extensions - excluded words
         zip_links = []
-
         for link in links:
-            if link['href'].endswith('.zip') and usaTag in link.text:
-                zip_links.append(link.text)
-            elif link['href'].endswith('.7z') and usaTag in link.text:
-                zip_links.append(link.text)
-
-        # print(zip_links)
-
-        cleaned_list = []
-
-        for link in zip_links:
-            if not any(w in link for w in excludedWords):
-                if '.7z' in link:
-                    link = link.rstrip('.7z')
-                    cleaned_list.append(link)
-                elif '.zip' in link:
-                    link = link.rstrip('.zip')
-                    cleaned_list.append(link)
-
-        # print(cleaned_list)
+            if not any(w in link.text for w in filteredWords) and any(extension in link['href'] for extension in accepted_file_extensions) and usaTag in link.text:
+                zip_links.append(Path(link.text).stem)
 
         libraryPath = f'{roms_dir}romlibrary'
+        print(f'Library path: {libraryPath}')
         if not os.path.exists(libraryPath):
             os.makedirs(libraryPath)
         else:
-            print(f"Library path {libraryPath} already set.")
+            print(f'Library path already exists, skipping mkdir.')
 
-        print(url)
-
-        # Based on your actual url that you use to download games off of, change the strings below to match your urls. I am not able to openly post the links I use due to potential legal issues of downloading roms off the internet.
         if snes_url in url:         
             consolePath = libraryPath + '/snes'
-            print(consolePath)
         elif nes_url in url:
             consolePath = libraryPath + '/nes'
-            print(consolePath)
         elif n64_url in url:
             consolePath = libraryPath + '/n64'
-            print(consolePath)
         elif gb_url in url:
             consolePath = libraryPath + '/gb'
-            print(consolePath)
         elif gbc_url in url:
             consolePath = libraryPath + '/gbc'
-            print(consolePath)
         elif gba_url in url:
             consolePath = libraryPath + '/gba'
-            print(consolePath)
         elif gc_url in url:
             consolePath = libraryPath + '/gc'
-            print(consolePath)
         elif nds_url in url:
             consolePath = libraryPath + '/nds'
-            print(consolePath)
         elif n3ds_url in url:
             consolePath = libraryPath + '/n3ds'
-            print(consolePath)
         elif wii_url in url:
             consolePath = libraryPath + '/wii'
-            print(consolePath)
         elif wiiu_url in url:
             consolePath = libraryPath + '/wiiu'
-            print(consolePath)
-        elif ps2_url in url:
-            consolePath = libraryPath + '/ps2'
-            print(consolePath)
-        elif psp_url in url:
-            consolePath = libraryPath + '/psp'
-            print(consolePath)
         elif psx_url in url:
             consolePath = libraryPath + '/psx'
-            print(consolePath)
+        elif ps2_url in url:
+            consolePath = libraryPath + '/ps2'
+        elif psp_url in url:
+            consolePath = libraryPath + '/psp'
+        elif psvita_url in url:
+            consolePath = libraryPath + '/psvita'
         elif dc_url in url:
             consolePath = libraryPath + '/dreamcast'
-            print(consolePath)
         else:
-            print("URL not recognized. Can't create console path.")
+            print('URL not recognized. Can\'t create console path.')
+
+        print(f'Console path: {consolePath}')
 
         if not os.path.exists(consolePath):
             os.makedirs(consolePath)
         else:
-            print("Console path already exists.")
+            print('Console path dir already exists, skipping mkdir.')
 
-        # Based on your actual url that you use to download games off of, change the url below to match your urls. I am not able to openly post the links I use due to potential legal issues of downloading roms off the internet.
-        print(f"Creating download files for {len(cleaned_list)} games")
-        for title in cleaned_list:
+        print(f'Creating download files for {len(zip_links)} games')
+        
+        for title in zip_links:
             with open(consolePath + '/' + title + '.py', 'w') as f:
                 f.write(
                     'import os\n'
-                    'import subprocess\n'
                     'import requests\n'
                     'from bs4 import BeautifulSoup\n'
+                    'from pathlib import Path\n'
                     'import zipfile\n'
                     'import py7zr\n'
                     '\n'
-                    '# Based on your actual url that you use to download games off of, change the url below to match your urls. I am not able to openly post the links I use due to potential legal issues of downloading roms off the internet.\n'
                     'if \'snes\' in str(os.path.abspath(__file__)):\n'
                     f'   url = \'{snes_url}\'\n'
                     f'   file_path = \'{roms_dir}snes\'\n'
@@ -177,11 +150,14 @@ def read_files(url):
                     'elif \'psp\' in str(os.path.abspath(__file__)):\n'
                     f'   url = \'{psp_url}\'\n'
                     f'   file_path = \'{roms_dir}psp\'\n'
+                    'elif \'psvita\' in str(os.path.abspath(__file__)):\n'
+                    f'   url = \'{psvita_url}\'\n'
+                    f'   file_path = \'{roms_dir}psvita\'\n'
                     'elif \'dreamcast\' in str(os.path.abspath(__file__)):\n'
                     f'   url = \'{dc_url}\'\n'
                     f'   file_path = \'{roms_dir}dreamcast\'\n'
                     'else:\n'
-                    '   print("Current directory not recognized.")\n'
+                    '   print(\'Current directory not recognized.\')\n'
                     '\n'
                     'response = requests.get(url)\n'
                     '\n'
@@ -191,22 +167,18 @@ def read_files(url):
                     '\n'
                     '   zip_links = []\n'
                     '   for link in links:\n'
-                    '       if link[\'href\'].endswith(\'.zip\') or link[\'href\'].endswith(\'.7z\'):\n'
+                   f'       if any(extension in link[\'href\'] for extension in {accepted_file_extensions}):\n'
                     '           zip_links.append(link)\n'
                     '\n'
                     '   for link in zip_links:\n'
-                    '       if link.text.endswith(\'.zip\'):\n'
-                    '           link_text = link.text.rstrip(\'.zip\')\n'
-                    '       elif link.text.endswith(\'.7z\'):\n'
-                    '           link_text = link.text.rstrip(\'.7z\')\n'
-                    '\n'
+                    '       link_text = Path(link.text).stem\n'
                     '       if link_text in str(os.path.basename(__file__)).rstrip(\'.py\'):\n'
                     '           full_url = url + \'/\' + link[\'href\']\n'
                     '           download_response = requests.get(full_url)\n'
                     '           if download_response.status_code == 200:\n'
                     '               with open(file_path + \'/\' + link.text, \'wb\') as f:\n'
                     '                   f.write(download_response.content)\n'
-                    '               print("Download successful.")\n'
+                    '               print(\'Download successful.\')\n'
                     '\n'
                     '               downloaded_file = file_path + \'/\' + link.text\n'
                     '               if downloaded_file.endswith(\'.zip\'):\n'
@@ -219,64 +191,50 @@ def read_files(url):
                     '                   os.remove(downloaded_file)\n'
                     '               break\n'
                     '           else:\n'
-                    '               print("Failed to download.")\n'
+                    '               print(\'Failed to download.\')\n'
                     '               break\n'
                     'else:\n'
-                    '   print("Game not found in HTML.")\n'
+                    '   print(\'Game not found in HTML.\')\n'
                 )
                 
     else:
-        print("Request failed..")
+        print('Request failed..')
 
 
 def addLibrary():
-    xmlPath = os.path.expanduser('~/ES-DE/custom_systems/es_systems.xml')
+    xmlPath = os.path.expanduser(systems_xml_path)
+    print(f'Systems XML Path: {xmlPath}')
     with open(xmlPath, 'r') as xml_file:
         xml_content = xml_file.read()
-    #print(xml_content)
 
-    libraryContent = f"""
+    libraryContent = f'''
         <system>
             <name>romlibrary</name>
             <fullname>ROM Library</fullname>
             <path>%ROMPATH%/romlibrary</path>
             <extension>.py</extension>
-            <command>/bin/bash {os.path.expanduser('~/rom-library/download.sh')} "%ROM%"</command>
+            <command>/bin/bash {os.path.expanduser('~/rom-library/download.sh')} '%ROM%'</command>
             <theme>library</theme>
         </system>
-    """
+    '''
 
     startTag = xml_content.find('<systemList>')
     endTag = xml_content.find('</systemList>')
 
     if libraryContent not in xml_content:
-        # -1 means string was not found. str.find(), str.index(), str.rfind() returns -1 if substring or character is not found
         if startTag != -1 and endTag != -1:
             xml_content_modified = xml_content[:endTag] + libraryContent + xml_content[endTag:]
-            #print(xml_content_modified)
-
             with open(xmlPath, 'w') as xml_file:
                 xml_file.write(xml_content_modified)
         else:
-            print("<systemList> and </systemList> tags were not found.")
+            print('Unable to find valid es_systems.xml')
     else:
-        print("Library is already added to es_systems.xml.")
+        print('<system> entry already added to es_systems.xml.')
 
 
+# Initialize ROM Library in es_systems.xml then export download ROM python scripts for each system
 addLibrary()
 
-read_files(snes_url)
-read_files(nes_url)
-read_files(n64_url)
-read_files(gb_url)
-read_files(gbc_url)
-read_files(gba_url)
-read_files(gc_url)
-read_files(nds_url)
-read_files(n3ds_url)
-read_files(wii_url)
-read_files(wiiu_url)
-read_files(psx_url)
-read_files(ps2_url)
-read_files(psp_url)
-read_files(dc_url)
+for url in rom_urls:
+    if url not in 'XXXXXXX':
+        read_files(snes_url)
